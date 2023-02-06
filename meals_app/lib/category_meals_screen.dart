c1ffff09260acd938bb5e3fff6d2d494311b2eac
Ibrahim-Lokman <ibrahimlokman.bd@gmail.com>
2023-02-06 18:43:20 +0600

    push replaced by pushNamed, Modal. Routes added in main.dart, MaterialPageRoute replaced.

--- meals_app/lib/category_meals_screen.dart
+++ meals_app/lib/category_meals_screen.dart
@@ -3,13 +3,19 @@ import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
 
 class CategoryMealsScreen extends StatelessWidget {
+ 
+ /*
   final String categoryId;
   final String categoryTitle;
 
   CategoryMealsScreen(this.categoryId, this.categoryTitle);
-
+*/
   @override
   Widget build(BuildContext context) {
+
+    final routesArgs = ModalRoute.of(context)!.settings.arguments as Map<String,String>;
+    final  String? categoryId = routesArgs['id'];
+    final  String categoryTitle = routesArgs['title']!;
     return Scaffold(
       appBar: AppBar(
         title: Text(
