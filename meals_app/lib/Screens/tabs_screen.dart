f7c7f293cd1ca7eeabf4379ab2ffb25b6e5a3675
Ibrahim-Lokman <ibrahimlokman.bd@gmail.com>
2023-02-07 19:04:22 +0600

    Custom drawer added for tab

--- meals_app/lib/Screens/tabs_screen.dart
+++ meals_app/lib/Screens/tabs_screen.dart
@@ -3,6 +3,7 @@ import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:meals_app/Screens/categories_screen.dart';
 import 'package:meals_app/Screens/favorites_screen.dart';
+import 'package:meals_app/Widgets/main_drawer.dart';
 
 class TabScreen extends StatefulWidget {
 
@@ -11,37 +12,47 @@ class TabScreen extends StatefulWidget {
 }
 
 class _TabScreenState extends State<TabScreen> {
+  final List<Map<String, dynamic>> _pages = [
+    //as Widget
+   {'page': CategoriesScreen() as Widget, 'title': 'Categories',},
+   {'page': FavoritesScreen() as Widget, 'title': 'My Favorites',},
+  ];
+
+  int _selectedPageIndex = 0;
+
+  void _selectPage(int index){
+      setState(() {
+        _selectedPageIndex = index;
+      });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 2,
-      //initialIndex: 1,
-      child: Scaffold(
+    return Scaffold(
         appBar: AppBar(
-          title: Text('Meals'),
-          bottom: TabBar(
-            tabs: <Widget>[
-              Tab(
-                icon: Icon(
-                  Icons.category,
-                  ),
-                text: 'Categories',
+          title: Text(_pages[_selectedPageIndex]['title']),
+         ),
+          drawer: MainDrawer(),
+          body: _pages[_selectedPageIndex]['page'],
+          bottomNavigationBar: BottomNavigationBar(
+            onTap : _selectPage,
+            backgroundColor:Theme.of(context).primaryColor,
+            unselectedItemColor: Colors.white,
+            selectedItemColor: Color.fromARGB(255, 138, 255, 142),
+            currentIndex: _selectedPageIndex,
+          //  type: BottomNavigationBarType.shifting,
+            items: [
+              BottomNavigationBarItem(
+                backgroundColor: Theme.of(context).primaryColor,
+                icon: Icon(Icons.category),
+                label: 'Categories',
                 ),
-              Tab(
-                icon: Icon(
-                  Icons.star,
-                  ),
-                text: 'Favorites',
+              BottomNavigationBarItem(
+                icon: Icon(Icons.star),
+                label: 'Favorites',
                 ),
             ],
           ),
-         ),
-          body: TabBarView(children: <Widget>[
-            CategoriesScreen(),
-            FavoritesScreen() 
-          ],
-        ),
-      ),
     );
   }
 }
