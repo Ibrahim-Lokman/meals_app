83454d7f4c26428dd01d7dc11aaebc961e85494b
Ibrahim-Lokman <ibrahimlokman.bd@gmail.com>
2023-02-08 11:42:47 +0600

    pushReplacementNamed method used.

--- meals_app/lib/Screens/favorites_screen.dart
+++ meals_app/lib/Screens/favorites_screen.dart
@@ -3,6 +3,7 @@ import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
 
 class FavoritesScreen extends StatelessWidget {
+  
 
   @override
   Widget build(BuildContext context) {
