8b8b0a4dc0fc0145435c0a9035ed326de5126c36
Ibrahim-Lokman <ibrahimlokman.bd@gmail.com>
2023-04-13 22:41:53 +0600

    Riverpod version initial commit

--- meals_app/lib/Widgets/main_drawer.dart
+++ meals_app/lib/Widgets/main_drawer.dart
@@ -1,68 +1,83 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:meals_app/Screens/filters_screen.dart';
 
 class MainDrawer extends StatelessWidget {
+  const MainDrawer({super.key, required this.onSelectScreen});
 
-  Widget buildListTile(String title, IconData icon, Function()? tapHandler) {
-    return ListTile(
-            leading: Icon(
-              icon,
-              size: 26,
-            ),
-            title: Text(
-              title,
-              style: TextStyle(
-                fontFamily: 'RobotoCOndensed',
-                fontSize: 24,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            onTap:  tapHandler,
-          );
-  }
+  final void Function(String identifier) onSelectScreen;
 
   @override
   Widget build(BuildContext context) {
     return Drawer(
       child: Column(
-        children: <Widget>[
-          Container(
-            height: 120,
-            width: double.infinity,
-            padding: EdgeInsets.only(
-              top: 25,
-              left: 15,
+        children: [
+          DrawerHeader(
+            padding: const EdgeInsets.all(20),
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                colors: [
+                  Theme.of(context).colorScheme.primaryContainer,
+                  Theme.of(context)
+                      .colorScheme
+                      .primaryContainer
+                      .withOpacity(0.8),
+                ],
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+              ),
             ),
-            alignment: Alignment.centerLeft,
-            color: Theme.of(context).primaryColor,
-            child: Text(
-              'Cooking Up', 
-              style: TextStyle(
-                fontWeight: FontWeight.w900,
-                fontSize: 30,
-                color: Colors.white,
-              ),),
+            child: Row(
+              children: [
+                Icon(
+                  Icons.fastfood,
+                  size: 48,
+                  color: Theme.of(context).colorScheme.primary,
+                ),
+                const SizedBox(width: 18),
+                Text(
+                  'Cooking Up!',
+                  style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                        color: Theme.of(context).colorScheme.primary,
+                      ),
+                ),
+              ],
+            ),
           ),
-          SizedBox(height: 20),
-          buildListTile(
-            'Meals', 
-            Icons.restaurant,
-            () {
-              Navigator.of(context).pushReplacementNamed('/');
+          ListTile(
+            leading: Icon(
+              Icons.restaurant,
+              size: 26,
+              color: Theme.of(context).colorScheme.onBackground,
+            ),
+            title: Text(
+              'Meals',
+              style: Theme.of(context).textTheme.titleSmall!.copyWith(
+                    color: Theme.of(context).colorScheme.onBackground,
+                    fontSize: 24,
+                  ),
+            ),
+            onTap: () {
+              onSelectScreen('meals');
             },
           ),
-          buildListTile(
-            'Filters', 
-            Icons.settings,
-            () {
-               Navigator.of(context).pushNamed(FiltersScreen.routeName);
+          ListTile(
+            leading: Icon(
+              Icons.settings,
+              size: 26,
+              color: Theme.of(context).colorScheme.onBackground,
+            ),
+            title: Text(
+              'Filters',
+              style: Theme.of(context).textTheme.titleSmall!.copyWith(
+                    color: Theme.of(context).colorScheme.onBackground,
+                    fontSize: 24,
+                  ),
+            ),
+            onTap: () {
+              onSelectScreen('filters');
             },
           ),
-          
         ],
       ),
     );
-  } 
+  }
 }
--- meals_app/lib/main.dart
+++ meals_app/lib/main.dart
@@ -1,133 +1,28 @@
 import 'package:flutter/material.dart';
-import 'package:meals_app/Models/meal.dart';
-import 'package:meals_app/Screens/categories_screen.dart';
-import 'package:meals_app/Screens/category_meals_screen.dart';
-import 'package:meals_app/Screens/filters_screen.dart';
-import 'package:meals_app/Screens/meal_details_screen.dart';
-import 'package:meals_app/Screens/tabs_screen.dart';
-import './dummy_data.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:meals_app/screens/tabs.dart';
+
+final theme = ThemeData(
+  useMaterial3: true,
+  colorScheme: ColorScheme.fromSeed(
+    brightness: Brightness.dark,
+    seedColor: const Color.fromARGB(255, 131, 57, 0),
+  ),
+  textTheme: GoogleFonts.latoTextTheme(),
+);
 
 void main() {
-  runApp(MyApp());
+  runApp(App());
 }
 
-class MyApp extends StatefulWidget {
-  @override
-  State createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-
-  Map<String, bool> _filters = {
-    'gluten' : false,
-    'lactose' : false,
-    'vegetarian' : false,
-  };
-
-  List<Meal> _availableMeals = DUMMY_MEALS;
-  List<Meal> _favoriteMeals = [];
-  
-  void _setFilters(Map<String, bool> filterData){
-    setState(() {
-      _filters = filterData;
-      _availableMeals = DUMMY_MEALS.where((meal){
-        if(_filters['gluten']! && !meal.isGlutenFree){
-          return false;
-        }
-         if(_filters['lactose']! && !meal.isLactoseFree){
-          return false;
-        }
-         if(_filters['vegetarian']! && !meal.isVegetarian){
-          return false;
-        }
-        return true;
-      }).toList();
-    });
-  }
-
-  void _toggleFavorite(String mealId){
-    final existingIndex = 
-      _favoriteMeals.indexWhere(
-        (meal) => meal.id == mealId
-      );
-
-      if(existingIndex >= 0) {
-        setState((){
-          _favoriteMeals.removeAt(existingIndex);
-        });
-      } else {
-        setState((){
-          _favoriteMeals.add(
-            DUMMY_MEALS.firstWhere(
-              (meal) => meal.id == mealId
-            ),
-          );
-        });
-      }
-  }
-
-  bool _isMealFavorite(String id) {
-    return _favoriteMeals.any(
-      (meal) => meal.id == id
-    );
-  }
-
+class App extends StatelessWidget {
+  const App({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Meals App',
-      theme: ThemeData(
-        primarySwatch: Colors.pink,
-        accentColor: Colors.blue,
-        canvasColor: Color.fromRGBO(255, 254, 229, 1),
-        fontFamily: 'Raleway',
-        textTheme: ThemeData.light().textTheme.copyWith(
-          bodyText1 : TextStyle(
-            color : Color.fromRGBO(20, 51, 51, 1)
-          ),
-          bodyText2 : TextStyle(
-            color : Color.fromRGBO(20, 51, 51, 1)
-          ),
-          subtitle1 : TextStyle(
-            fontSize: 24,
-            fontFamily: 'RobotoCondensed',
-          ),
-        ),
-      ),
-     // home: CategoriesScreen(),
-      initialRoute: '/',
-      routes: {
-        '/' :(context) => TabScreen(_favoriteMeals),
-        CategoryMealsScreen.routeName : (context) => CategoryMealsScreen(_availableMeals),
-        MealDetailsScreen.routeName : (context) => MealDetailsScreen(_toggleFavorite, _isMealFavorite),
-        FiltersScreen.routeName: (context) => FiltersScreen(_filters, _setFilters),
-
-      },
-
-      /*
-      onGenerateRoute: (settings) {
-        print(settings.arguments);
-
-        /*
-
-        if(settings.name == '/meal-details') {
-          return  .......;
-        } else if(settings.name == '/something-else') {
-          return  ...;
-        }
-        
-        */
-
-        return MaterialPageRoute(
-          builder: (context) => CategoriesScreen(),);
-      },
-      onUnknownRoute: (settings) {
-          return MaterialPageRoute(
-          builder: (context) => CategoriesScreen(),);
-      },
-
-      */
+      theme: theme,
+      home: const TabsScreen(),
     );
   }
 }
